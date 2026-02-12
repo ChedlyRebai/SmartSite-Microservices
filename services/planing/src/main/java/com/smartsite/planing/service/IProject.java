301be3b74ddd6c897adbae02668ccd4e3df49f60
rebaiChedly <107418294+ChedlyRebai@users.noreply.github.com>
2026-02-12 14:03:40 +0100

    add project interface function

--- services/planing/src/main/java/com/smartsite/planing/service/IProject.java
+++ services/planing/src/main/java/com/smartsite/planing/service/IProject.java
@@ -1,5 +1,8 @@
 package com.smartsite.planing.service;
 
 public interface IProject {
-    
+    Project getProjectById(Long id);
+    void DeleteProject(Long id);
+    Project updateProject(Project project,Long id);
+    Project AddProject(Project project);
 }
--- services/planing/src/main/java/com/smartsite/planing/service/IResourceNeed.java
+++ services/planing/src/main/java/com/smartsite/planing/service/IResourceNeed.java
@@ -1,5 +1,8 @@
 package com.smartsite.planing.service;
 
 public interface IResourceNeed {
-    
+    ResourceNeed addRessource(ResourceNeed resourceNeed);
+    void DeleteRessource(long id);
+    ResourceNeed updateResourceNeed(ResourceNeed ressourceNeed,Long id);
+    ResourceNeed getById(Long id);
 }
