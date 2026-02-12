fd516ffcd444b033dcaeee1f3697eb40fae52f69
rebaiChedly <107418294+ChedlyRebai@users.noreply.github.com>
2026-02-12 10:17:20 +0100

    add TaskSTatus enum

--- services/planing/src/main/java/com/smartsite/planing/entity/Task.java
+++ services/planing/src/main/java/com/smartsite/planing/entity/Task.java
@@ -1,5 +1,8 @@
+package com.smartsite.planing.entity;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -16,8 +19,6 @@ public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    
     private String title;
 
 
