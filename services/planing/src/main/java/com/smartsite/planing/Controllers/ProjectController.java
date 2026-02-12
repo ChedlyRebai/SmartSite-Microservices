3e5def1efae57b3c357cb4556093db9fbd656bbc
rebaiChedly <107418294+ChedlyRebai@users.noreply.github.com>
2026-02-12 16:51:41 +0100

    update contollers

--- services/planing/src/main/java/com/smartsite/planing/Controllers/ProjectController.java
+++ services/planing/src/main/java/com/smartsite/planing/Controllers/ProjectController.java
@@ -1,5 +1,11 @@
 package com.smartsite.planing.Controllers;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.NoArgsConstructor;
 
+@RestController
+@RequestMapping("/api/project")
+@NoArgsConstructor
 public class ProjectController {
     
 }
