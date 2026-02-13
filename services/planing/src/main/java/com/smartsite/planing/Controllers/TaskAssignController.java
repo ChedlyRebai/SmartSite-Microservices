ba1fd923d08df8d807fdd18b6deabca0f26dab2c
rebaiChedly <107418294+ChedlyRebai@users.noreply.github.com>
2026-02-13 01:10:33 +0100

    feat: implement TaskAssignController with create and retrieve functionality for TaskAssigne

--- services/planing/src/main/java/com/smartsite/planing/Controllers/TaskAssignController.java
+++ services/planing/src/main/java/com/smartsite/planing/Controllers/TaskAssignController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.smartsite.planing.domain.entity.Task;
+import com.smartsite.planing.domain.entity.TaskAssigne;
 import com.smartsite.planing.service.ITaskAssigne;
 
 import lombok.NoArgsConstructor;
@@ -13,6 +14,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -27,10 +31,20 @@ public class TaskAssignController {
     }
 
     @PostMapping("/{taskId}")
-    public ResponseEntity<Task> postTaskAssign(@RequestBody Task task,@PathVariable Long taskId) {
-        return ResponseEntity.ok().body(null);
+    public ResponseEntity<TaskAssigne> postTaskAssign(@RequestBody TaskAssigne taskAssigne,@PathVariable Long taskId) {
+        return ResponseEntity.ok(this.taskAssigneService.create(taskId, taskAssigne));
     }
+
+    @GetMapping("/{taskid}")
+    public ResponseEntity<TaskAssigne> getTaskAssign(@PathVariable Long taskid) {
+        return ResponseEntity.ok(this.taskAssigneService.getById(taskid));
+    }
+
     
+    
+    
+
+
 
     
     
