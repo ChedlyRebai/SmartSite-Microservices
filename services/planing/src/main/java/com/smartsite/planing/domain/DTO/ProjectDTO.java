4280108947084c97f38b6cd70d5833024631d25b
rebaiChedly <107418294+ChedlyRebai@users.noreply.github.com>
2026-02-16 23:50:27 +0100

    feat: create ProjectDTO class for project data representation

---
+++ services/planing/src/main/java/com/smartsite/planing/domain/DTO/ProjectDTO.java
@@ -0,0 +1,13 @@
+package com.smartsite.planing.domain.DTO;
+
+import java.time.LocalDate;
+
+
+
+public class ProjectDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private LocalDate plannedStart;
+
+}
