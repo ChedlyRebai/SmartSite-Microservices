8dbfa06eb84dabc8e0b31fcc1db2b4691d5ed8f9
rebaiChedly <107418294+ChedlyRebai@users.noreply.github.com>
2026-02-16 23:50:53 +0100

    feat: add Lombok annotations to ProjectDTO for boilerplate reduction

--- services/planing/src/main/java/com/smartsite/planing/domain/DTO/ProjectDTO.java
+++ services/planing/src/main/java/com/smartsite/planing/domain/DTO/ProjectDTO.java
@@ -2,8 +2,16 @@ package com.smartsite.planing.domain.DTO;
 
 import java.time.LocalDate;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProjectDTO {
     private Long id;
     private String name;
